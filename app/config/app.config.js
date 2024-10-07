6c120647ddf83127e84a3419c05a505d250284d9
FardinOA <afnan.eu.cse@gmail.com>
2024-10-07 16:45:32 +0600

    refactor: cors origin

--- app/config/app.config.js
+++ app/config/app.config.js
@@ -14,8 +14,10 @@ const app = express();
 // Allow Cross-Origin requests
 app.use(
   cors({
-    origin: ["http://localhost/5173"],
+    origin: ["http://localhost:5173"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
